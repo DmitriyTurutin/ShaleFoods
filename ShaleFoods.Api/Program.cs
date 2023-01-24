11920106698caaaa26eda08b9fa142ac5e601339
Dima Turutin <qwe068776@gmail.com>
2023-01-25 01:38:07 +0300

    Init commit

---
+++ ShaleFoods.Api/Program.cs
@@ -0,0 +1,28 @@
+using ShaleFoods.Api;
+using ShaleFoods.Application;
+using ShaleFoods.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services
+        .AddPresentation()
+        .AddApplication()
+        .AddInfrastructure();
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
