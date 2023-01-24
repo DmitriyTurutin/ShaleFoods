f8efe3ba0517a6fd9904a2833782b284ae064577
Dima Turutin <qwe068776@gmail.com>
2023-01-25 02:48:27 +0300

    Folders refactoring

--- src/ShaleFoods.Api/DependencyInjection.cs
+++ src/ShaleFoods.Api/DependencyInjection.cs
@@ -1,12 +1,15 @@
 namespace ShaleFoods.Api;
 
-public static class DependencyInjection
+public static class DependencyInjection 
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+
         return services;
+
     }
 }
+    
--- src/ShaleFoods.Api/Program.cs
+++ src/ShaleFoods.Api/Program.cs
@@ -1,12 +1,11 @@
 using ShaleFoods.Api;
-using ShaleFoods.Application;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 {
     builder.Services
-        .AddPresentation()
-        .AddApplication()
-        .AddInfrastructure();
+        .AddPresentation();
 }
 
 var app = builder.Build();
